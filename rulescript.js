72c1fb7d4a506f4d6738cd4482f2037e813b805d
vancedust <110260252+vancedust@users.noreply.github.com>
2023-01-06 21:30:56 -0600

    Changes for Beta Website

---
+++ rulescript.js
@@ -0,0 +1,34 @@
+// This function creates a new tab with dsicord link
+function discord() {
+    window.open("https://discord.gg/J3vfCXuGpj", "_blank");
+}
+
+// This function creates a new email
+function email() {
+    window.open('mailto:ephemeralgate@gmail.com');
+}
+
+// This function goes to rules page
+function rule_link() {
+    window.open("rules.html", "_self");
+}
+
+// This function goes to home page
+function home_link() {
+    window.open("index.html", "_self");
+}
+
+// This function goes to figure builder
+function fig_link() {
+    window.open("figbuilder.html", "_blank");
+}
+
+// This function downloads rules
+function ruledwn() {
+    window.open("ephemeral gate rules of play v0.20.pdf");
+}
+
+// This function downloads rules
+function abdwn() {
+    window.open("ephemeral gate ability guide w0.32.pdf");
+}
